5262e93c06b8c3c6bca952b0fbd7916e22075f06
Johanlex <127138253+Johanlex20@users.noreply.github.com>
2026-08-01 11:33:24 -0500

    Actualiza Angular 17 -> 18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,27 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashBoardComponent } from './dash-board/dash-board.component';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { HomeComponent } from './home/home.component';
 import { MessagesComponent } from './messages/messages.component';
 import { RegistrosComponent } from './registros/registros.component';
 import { UserComponent } from './user/user.component';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    DashBoardComponent,
-    HomeComponent,
-    MessagesComponent,
-    RegistrosComponent,
-    UserComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        DashBoardComponent,
+        HomeComponent,
+        MessagesComponent,
+        RegistrosComponent,
+        UserComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
